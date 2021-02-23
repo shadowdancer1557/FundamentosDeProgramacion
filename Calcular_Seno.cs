66b6fb2ee956f538a5c2cc722191aed96217a787
shadowdancer1557 <50499778+shadowdancer1557@users.noreply.github.com>
2021-02-23 00:33:43 -0500

    Add files via upload

---
+++ Calcular_Seno.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Calcular_Seno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 0.5;
+            int n = 1000;
+            double resultado = 0;
+            for(int i =0;i<=n; i++)
+            {
+                resultado += (Math.Pow(-1, i) / Factorial((2 * i) + 1)) * Math.Pow(x, (2 * i) + 1);
+            }
+            Console.WriteLine(resultado);
+        }
+
+
+        static double Factorial(double valor)
+        {
+            double total = 1;
+            for(int i = 1; i <= valor; i++)
+            {
+                total *= i;
+            }
+            return total;
+        }
+    }
+}
