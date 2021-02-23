550338f769530b65092ba168c5490634fb85533b
shadowdancer1557 <50499778+shadowdancer1557@users.noreply.github.com>
2021-02-23 00:31:05 -0500

    Add files via upload
    https://youtu.be/GaTVPvWBNuI

---
+++ Calcular_Euler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Euler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 3;
+            double n = 100;
+            double resultado = 0;
+
+            for(int i = 0; i<=n; i++)
+              {
+                resultado += (Math.Pow(x, i)) / Factorial(i);
+                Console.WriteLine(resultado);
+              }
+
+
+        }
+
+        static double Factorial(double valor)
+        {
+            double total = 1;
+            for(int i = 1; i <= valor; i++)
+            {
+                total *= i;
+            }
+            return total;
+        }
+    }
+}
